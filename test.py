b0ef8d459412c0ca06ce289a355f7442b4e970d8
Joshua Draper <josh.drapergame@gmail.com>
2020-06-14 20:07:29 -0600

    completed algorithm short answers and added tests

---
+++ test.py
@@ -0,0 +1,34 @@
+# def test1(a):
+
+#     count = 0
+
+#     n = 500
+
+#     while (a < n * n * n):
+#         a = a + n * n
+#         count += 1
+#     print(count)
+
+
+# test1(99000000)
+
+# def test2(n):
+#     sum = 0
+#     for i in range(n):
+#       j = 1
+#       while j < n:
+#         j *= 2
+#         sum += 1
+#     print(sum)
+
+# test2(100)
+
+# def bunnyEars(bunnies):
+
+#     if bunnies == 0:
+#         return 0
+
+#     return 2 + bunnyEars(bunnies-1)
+
+
+# bunnyEars(15)
